1ee352ebe60987aa72299d0029cb16f291428d8f
Danyang Wang <DAWA@simcorp.com>
2022-11-21 14:02:33 +0100

    add extra output.

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -6,7 +6,9 @@ namespace HomeWork1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
             Console.WriteLine("Feature branch 1 - development 1");
+            Console.WriteLine("Feature branch 1 - development 2");
         }
     }
 }
