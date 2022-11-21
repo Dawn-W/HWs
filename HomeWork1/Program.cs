6406b539cdbf81d4ee1fc729076fb4022e2162c6
Danyang Wang <DAWA@simcorp.com>
2022-11-21 13:59:00 +0100

    change output

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -6,8 +6,7 @@ namespace HomeWork1
     {
         static void Main(string[] args)
         {
-            //Output Hello World.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Feature branch 1 - development 1");
         }
     }
 }
